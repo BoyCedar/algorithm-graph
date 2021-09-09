d9ef8d3294fa22b0d340ecc072993bb22412ad2c
qixs <xiaosong_email@163.com>
2021-09-09 13:00:14 +0800

    选择排序代码

--- src/main/java/com/study/SelectSortTest.java
+++ src/main/java/com/study/SelectSortTest.java
@@ -4,5 +4,17 @@ public class SelectSortTest {
 
     public static void main(String[] args) {
 
+        int[] arr = new int[]{1,3,2,8,7,4,6};
+
+        int arrLength = arr.length;
+        for (int i = 0; i < arrLength; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
     }
 }
